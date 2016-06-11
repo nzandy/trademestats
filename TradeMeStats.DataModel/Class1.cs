3fa13ad80eecca5b804c9cd6f862f4df28cafcbf
= <=>
2016-06-11 21:25:43 +1200

    Adding Agencies DBSet.

--- TradeMeStats.DataModel/Class1.cs
+++ TradeMeStats.DataModel/Class1.cs
@@ -4,6 +4,6 @@ using TradeMeAPI.Models;
 namespace TradeMeStats.DataModel {
 	public class TrademeStatsContext : DbContext {
 		DbSet<RentalListing> RentalListings { get; set; }
-
+		DbSet<Agency> Angencies { get; set; }
 	}
 }
