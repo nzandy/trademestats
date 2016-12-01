6caa473ff3072b1d1470df4e1e290dd703507541
Andy <andrew.davies@telogis.com>
2016-12-01 13:45:36 +1300

    Add JSON Formatter to return JSON instead of XML. Add Entity Framework to packages. Add Connection string.

--- ListingApi/App_Start/WebApiConfig.cs
+++ ListingApi/App_Start/WebApiConfig.cs
@@ -1,24 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Web.Http;
 
-namespace ListingApi
-{
-    public static class WebApiConfig
-    {
-        public static void Register(HttpConfiguration config)
-        {
-            // Web API configuration and services
+namespace ListingApi {
+	public static class WebApiConfig {
+		public static void Register(HttpConfiguration config) {
+			// Web API configuration and services
 
-            // Web API routes
-            config.MapHttpAttributeRoutes();
+			// Web API routes
+			config.MapHttpAttributeRoutes();
 
-            config.Routes.MapHttpRoute(
-                name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
-            );
-        }
-    }
+			config.Routes.MapHttpRoute(
+				name: "DefaultApi",
+				routeTemplate: "api/{controller}/{id}",
+				defaults: new { id = RouteParameter.Optional }
+			);
+
+			config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+		}
+	}
 }
