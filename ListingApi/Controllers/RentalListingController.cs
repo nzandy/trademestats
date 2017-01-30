35316b6b953ee22a1fa049e3a0260db537d092c0
Andy <andrew.davies@telogis.com>
2017-01-30 15:13:23 +1300

    Added timer logic to listing monitor.

--- ListingApi/Controllers/RentalListingController.cs
+++ ListingApi/Controllers/RentalListingController.cs
@@ -1,7 +1,5 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Web.Http;
-using TradeMeAPI.Connectors;
 using TradeMeAPI.Models;
 using TradeMeStats.DataModel;
 
--- TrademeListingMonitor/Program.cs
+++ TrademeListingMonitor/Program.cs
@@ -16,13 +16,16 @@ namespace TrademeListingMonitor {
 			IEnumerable<RentalListing> rentalListings = rentalConnector.GetListings();
 			//save listings to database.
 			TrademeStatsRepository respository = new TrademeStatsRepository(new TrademeStatsContext());
+			Stopwatch sw = Stopwatch.StartNew();
+			sw.Start();
 			foreach (var rentalListing in rentalListings) {
 				trace.WriteLine($"Adding listing ID: {rentalListing.ListingId}");
 				respository.AddRentalListing(rentalListing);
 			}
 
 			respository.SaveChanges();
-			trace.WriteLine("Finished fetching listings, press enter to exit.");
+			sw.Stop();
+			trace.WriteLine($"Finished fetching listings, took {sw.Elapsed.TotalSeconds} seconds. Press enter to exit.");
 			Console.ReadLine();
 		}
 }
